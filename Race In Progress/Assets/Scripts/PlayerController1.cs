8ca3c7a60c45c12ebfe571f39cb7192c3f3a72b7
Natasha <67800233+Kanonerka@users.noreply.github.com>
2020-11-16 17:29:47 +0300

    Написаны базовые скрипты движения

---
+++ Race In Progress/Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float angle = 2;
+    public Transform body;
+    private float x;
+    private float z;
+    private float rotX = 0.0f;
+    void Start()
+    {
+        Vector3 rotation = transform.localRotation.eulerAngles;
+        rotX = rotation.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        //float x = Input.GetAxis("Horizontal");
+        //float z = Input.GetAxis("Vertical");
+        //Vector3 move = transform.forward;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            controller.Move(body.forward * speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            rotX -= angle;
+            body.rotation = Quaternion.Euler(0, rotX, 0);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            rotX += angle;
+            body.rotation = Quaternion.Euler(0,rotX,0);
+        }
+        
+
+        //controller.Move(move*speed*Time.deltaTime);
+        //body.Rotate(Vector3.up * 1);
+    }
+   
+}
---
+++ Race In Progress/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float angle = 2;
+    public Transform body;
+    private float x;
+    private float z;
+    private float rotX = 0.0f;
+    void Start()
+    {
+        Vector3 rotation = transform.localRotation.eulerAngles;
+        rotX = rotation.x;
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            controller.Move(body.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotX -= angle;
+            body.rotation = Quaternion.Euler(0, rotX, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotX += angle;
+            body.rotation = Quaternion.Euler(0, rotX, 0);
+        }
+
+    }
+}
