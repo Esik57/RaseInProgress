769e6927ef3f83938c3b8337ff8c9a224f523d6f
Natasha <67800233+Kanonerka@users.noreply.github.com>
2020-11-16 22:28:40 +0300

    Добавлена возможность закладывать мины

--- Race In Progress/Assets/Scripts/PlayerController1.cs
+++ Race In Progress/Assets/Scripts/PlayerController1.cs
@@ -6,20 +6,27 @@ public class PlayerController1 : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
-    public float angle = 2;
+    public float angle = 2f;
     public Transform body;
     private float x;
     private float z;
     private float rotX = 0.0f;
+
+    public bool isActive;
+    public GameObject bomb;
+    public float delay = 3f;
+    public Transform dropSpot;
 
     public float gravity = -10f;
     Vector3 velocity;
     public Transform groundCheck;
     public float groundDistance = 0.3f;
     public LayerMask groundMask;
-    bool isGrounded;
+    private bool isGrounded;
+
     void Start()
     {
+        isActive = true;
         Vector3 rotation = transform.localRotation.eulerAngles;
         rotX = rotation.x;
         Cursor.lockState = CursorLockMode.Locked;
@@ -27,27 +34,51 @@ public class PlayerController1 : MonoBehaviour
     }
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if (isGrounded && velocity.y < 0)
+        if (isActive)
         {
-            velocity.y = -2f;
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                controller.Move(body.forward * speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                rotX -= angle;
+                body.rotation = Quaternion.Euler(0, rotX, 0);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rotX += angle;
+                body.rotation = Quaternion.Euler(0, rotX, 0);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                bomb.SetActive(true);
+                bomb.transform.position = dropSpot.position;
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * 8f * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.W))
-        {
-            controller.Move(body.forward * speed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            rotX -= angle;
-            body.rotation = Quaternion.Euler(0, rotX, 0);
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            rotX += angle;
-            body.rotation = Quaternion.Euler(0,rotX,0);
-        }
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * 8f * Time.deltaTime);
     }
-   
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Bomb"))
+        {
+            isActive = false;
+            StartCoroutine("Waiting");
+        }
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(delay);
+        isActive = true;
+    }
 }
--- Race In Progress/Assets/Scripts/PlayerController2.cs
+++ Race In Progress/Assets/Scripts/PlayerController2.cs
@@ -6,46 +6,78 @@ public class PlayerController2 : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
-    public float angle = 2;
+    public float angle = 2f;
     public Transform body;
     private float x;
     private float z;
     private float rotX = 0.0f;
+
+    private bool isActive;
+    public GameObject bomb;
+    public float delay = 3f;
+    public Transform dropSpot;
 
     public float gravity = -10f;
     Vector3 velocity;
     public Transform groundCheck;
     public float groundDistance = 0.3f;
     public LayerMask groundMask;
-    bool isGrounded;
+    private bool isGrounded;
 
     void Start()
     {
+        isActive = true;
         Vector3 rotation = transform.localRotation.eulerAngles;
         rotX = rotation.x;
     }
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if(isGrounded && velocity.y < 0)
+        if(isActive)
         {
-            velocity.y = -2f;
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                controller.Move(body.forward * speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rotX -= angle;
+                body.rotation = Quaternion.Euler(0, rotX, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rotX += angle;
+                body.rotation = Quaternion.Euler(0, rotX, 0);
+            }
+
+            if(Input.GetKey(KeyCode.DownArrow))
+            {
+                bomb.SetActive(true);
+                bomb.transform.position = dropSpot.position;
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * 8f * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Bomb"))
         {
-            controller.Move(body.forward * speed * Time.deltaTime);
+            isActive = false;
+            StartCoroutine("Waiting");
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rotX -= angle;
-            body.rotation = Quaternion.Euler(0, rotX, 0);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rotX += angle;
-            body.rotation = Quaternion.Euler(0, rotX, 0);
-        }
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * 8f * Time.deltaTime);
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(delay);
+        isActive = true;
     }
 }
