d478fb7a21a1a68653e1ad4944b01f2a0acb5ab1
SocratesIgnotum <74509311+SocratesIgnotum@users.noreply.github.com>
2020-11-17 23:32:07 +0300

    Слияние

---
+++ Race In Progress 2/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float angle = 2f;
+    public Transform body;
+    private float x;
+    private float z;
+    private float rotX = 0.0f;
+
+    public GameObject newBomb;
+    private bool isActive;
+    //public GameObject bomb;
+    public float delay = 3f;
+    public Transform dropSpot;
+    private bool dropped = false;
+    public float BombDelay = 5;
+
+    public float gravity = -10f;
+    Vector3 velocity;
+    public Transform groundCheck;
+    public float groundDistance = 0.3f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    void Start()
+    {
+        isActive = true;
+        Vector3 rotation = transform.localRotation.eulerAngles;
+        rotX = rotation.x;
+    }
+    void Update()
+    {
+        if(isActive)
+        {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                controller.Move(body.forward * speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rotX -= angle;
+                body.rotation = Quaternion.Euler(0, rotX, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rotX += angle;
+                body.rotation = Quaternion.Euler(0, rotX, 0);
+            }
+
+            if(Input.GetKey(KeyCode.DownArrow) && !dropped)
+            {
+                //bomb.SetActive(true);
+                //bomb.transform.position = dropSpot.position;
+
+                Rigidbody clone;
+                clone = (Instantiate(newBomb, dropSpot.position, dropSpot.rotation)).GetComponent<Rigidbody>();
+
+                dropped = true;
+                StartCoroutine("BombDropped");
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * 8f * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Bomb"))
+        {
+            isActive = false;
+            StartCoroutine("Waiting");
+        }
+    }
+    IEnumerator BombDropped()
+    {
+        yield return new WaitForSeconds(BombDelay);
+        dropped = false;
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(delay);
+        isActive = true;
+    }
+}
