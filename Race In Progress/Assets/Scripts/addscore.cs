382a955fb8fe4ab0d31951278486886d3b159335
Zlata Bilko <Kira0saygo@gmail.com>
2020-11-18 18:09:57 +0300

    повторная отправка шрифтов

--- Race In Progress/Assets/Scripts/addscore.cs
+++ Race In Progress/Assets/Scripts/addscore.cs
@@ -2,17 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class score : MonoBehaviour
+public class addscore : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        public int Score
-        void OnGUI()
-    {
-        OnGUI.Label(new Rext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) "Счет" Score);
-
-    }
         
     }
 
